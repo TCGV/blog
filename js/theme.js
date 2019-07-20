6ad25ceb6321c1f95f8ea78e4eac2f436d07c37b
Thomas Vilhena <thomas.vilhena@gmail.com>
2019-07-20 12:56:20 -0300

    Positioning footer

--- js/theme.js
+++ js/theme.js
@@ -5,18 +5,23 @@ $(function () {
     setInterval(positionFooter, 250);
 
     function positionFooter() {
+
         var $footer = $('.footer');
         var _fixed = 'navbar-fixed-bottom';
         
-        if (Math.round($(window).height()) > Math.round(getBottom($footer))) {
-            $footer.addClass(_fixed);
-        } else {
+        var h1 = Math.round($(window).height());
+        var h2 = Math.round(getBottom($footer));
+        if (h1 != h2) {
             $footer.removeClass(_fixed);
+            if (h1 > h2) {
+                $footer.addClass(_fixed);
+            }
         }
     
         function getBottom($el) {
-            return parseInt($el.offset().top + $el.outerHeight());
+            return $el.offset().top + $el.outerHeight();
         }
+        
     }
 
 });
